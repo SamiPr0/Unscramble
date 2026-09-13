6742de5b69763612f47fc41e7d6d9840cc59d20a
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-09-13 12:28:47 +0200

    feat: handle last round of game

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.example.unscramble.data.MAX_NO_OF_WORDS
 import com.example.unscramble.data.SCORE_INCREASE
 import kotlinx.coroutines.flow.update
 
@@ -44,19 +45,31 @@ class GameViewModel : ViewModel() {
         val currentScrambledWord: String = "",
         val currentWordCount: Int = 1,
         val isGuessedWordWrong: Boolean = false,
-        val score: Int = 0
+        val score: Int = 0,
+        val isGameOver: Boolean = false
     )
     fun updateUserGuess(guessedWord: String){
         userGuess = guessedWord
     }
     private fun updateGameState(updatedScore: Int) {
-        _uiState.update { currentState ->
-            currentState.copy(
-                isGuessedWordWrong = false,
-                currentWordCount = currentState.currentWordCount.inc(),
-                currentScrambledWord = pickRandomWordAndShuffle(),
-                score = updatedScore
-            )
+        if (usedWords.size == MAX_NO_OF_WORDS) {
+            // Last round
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        } else {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentWordCount = currentState.currentWordCount.inc(),
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    score = updatedScore
+                )
+            }
         }
     }
     fun checkUserGuess() {
